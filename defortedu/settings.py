bbde255c59112f2ba838823c009b1ced12d4d38c
aroyalprince <artistkprincekumar@gmail.com>
2025-07-25 19:03:19 +0530

    Configure static files for Render

--- defortedu/settings.py
+++ defortedu/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["dofortedu.onrender.com"]
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Application definition
 
 INSTALLED_APPS = [
@@ -49,7 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'defortedu.urls'
 
